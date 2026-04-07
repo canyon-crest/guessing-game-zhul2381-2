afc6e66f30b10603f91a5a35c57462d78bab6e50
zhul2381 <zhul2381@my.sduhsd.net>
2026-04-07 22:28:15 +0000

    h

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // add javascript here
+let guess = 0;
+let guessCount = 0;
+let totalWins = 0;
+const scores = [];
+
+document.getElementById("playBtn").addEventListener
+("click",play);
+
+function play(){
+    let range = 0;
+    let levels = document.getElementsByName("level");
+    for (let i=0; i<levels.length; i++){
+        if(levels[i].checked){
+            range = parseInt(levels[i].value);
+    }
+    levels[i].disabled = true;
+}
+    document.getElementById("msg").textContent = "Guess a number 1-" + range;
+    answer = Math.floor(Math.random()*range) +1;
+    guessCount = 0;
+
+    guessBtn.disabled = false;
+    giveUpBtn.disabled = false
+    playBtn.disabled = true;
+}
